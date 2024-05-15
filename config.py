d678a86bf305a6f20c0a6d7a50d68e7a5b1f5129
Софи <151864844+Sofa12345@users.noreply.github.com>
2024-05-15 15:54:34 +0300

    Update config.py

--- config.py
+++ config.py
@@ -17,7 +17,7 @@ SYSTEM_PROMPT = [{'role': 'system', 'text': 'Ты веселый собесед�
                                             'Поддерживай диалог.'
                                             'Изображай человека'}] 
 
-HOME_DIR = '/Users/Citi/Desktop/pythonYandex/BotZinka'
+HOME_DIR = '/home/student/fiinal'
 LOGS = f'{HOME_DIR}/logs.txt'
 DB_FILE = f'{HOME_DIR}/messages.db'
 
